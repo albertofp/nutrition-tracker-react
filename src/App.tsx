f477435d3bbd25042a1b88efe1b88e1350f06e00
Alberto Pluecker <albertopluecker@gmail.com>
2023-03-22 19:39:29 +0100

    TP3 - Primeiro commit com equeleto básico do App

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import './styles/App.scss'
+
+function App() {
+	return (
+		<body>
+			<Header />
+			<Main />
+			<Footer />
+		</body>
+	)
+}
+
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+	return (
+		<footer className='footer'>
+			<div>Por: Alberto F. Pluecker</div>
+		</footer>
+	)
+}
+
+export default Footer
