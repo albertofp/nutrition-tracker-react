a756787274aa60750541151d769e49332b09ca35
Alberto Pluecker <albertopluecker@gmail.com>
2023-04-17 18:07:13 +0200

    added button animations

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ type Props = {
 }
 
 const btnStyle =
-	'bg-transparent hover:bg-slate-900 text-teal-600 font-semibold py-2 px-4 border border-teal-800 hover:border-transparent rounded min-w-[60px]'
+	'bg-transparent hover:bg-slate-900 active:bg-transparent text-teal-600 font-semibold py-2 px-4 border border-teal-800 hover:border-transparent rounded min-w-[60px] transition-colors duration:200'
 
 function Button({ text, onClick }: Props) {
 	return (
