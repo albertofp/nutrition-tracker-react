3002808b2f9ce17b2edfb4352f4eba7631e2641b
Alberto Pluecker <albertopluecker@gmail.com>
2023-04-26 00:11:53 +0200

    Contact page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,42 @@
 import SectionTitle from './SectionTitle'
+import SupabaseIcon from '../assets/supabase-icon.svg'
+import TypeScriptIcon from '../assets/typescript-logo.svg'
+import ReactIcon from '../assets/react.svg'
+import TailwindIcon from '../assets/tailwindcss-icon.svg'
+import ViteIcon from '../assets/vite-logo.svg'
 
-type Props = {}
-
-function About({}: Props) {
+function About() {
 	return (
 		<div className='flex flex-col p-2'>
-			<div className='flex flex-col justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+			<div className='flex flex-col justify-between items-center gap-4 h-24 max-w-[1240px] mx-auto px-4'>
 				<SectionTitle title={'About'} />
 				<h3 className='text-sky-300'>
 					App to track and display your daily calorie and macro nutrient
 					consumption. Developed by Alberto F. Pluecker as a learning project on
-					React, Typescript, Tailwind and Supabase.{' '}
+					React, Typescript, Tailwind and Supabase.
 				</h3>
+				<div className='flex gap-4'>
+					<img
+						src={ReactIcon}
+						className='w-14'
+					/>
+					<img
+						src={ViteIcon}
+						className='w-14'
+					/>
+					<img
+						src={TypeScriptIcon}
+						className='w-14'
+					/>
+					<img
+						src={TailwindIcon}
+						className='w-14'
+					/>
+					<img
+						src={SupabaseIcon}
+						className='w-14'
+					/>
+				</div>
 			</div>
 		</div>
 	)
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
 import SectionTitle from './SectionTitle'
+import GithubIcon from '../assets/github-icon.svg'
+import LinkedInIcon from '../assets/linkedin-icon.svg'
 
 export default function Contact() {
 	return (
-		<div className='flex flex-col p-2'>
-			<div className='flex flex-col justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-				<SectionTitle title={'Contact'} />
-				<h3 className='text-sky-300'>Github link here</h3>
+		<div className='flex flex-col gap-6'>
+			<SectionTitle title={'Contact'} />
+			<div className='flex flex-col md:flex-row md:justify-between gap-4 justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+				<a
+					href='https://github.com/albertofp'
+					className='flex gap-4 items-center bg-gradient-to-b from-sky-800 to-sky-950 rounded-md p-2'
+				>
+					<img
+						src={GithubIcon}
+						alt='github icon'
+						className='w-12'
+					/>
+					<SectionTitle title='albertofp' />
+				</a>
+
+				<a
+					href='https://www.linkedin.com/in/alberto-pluecker/'
+					className='flex gap-4 items-center bg-gradient-to-b from-sky-800 to-sky-950 rounded-md p-2'
+				>
+					<img
+						src={LinkedInIcon}
+						alt='github icon'
+						className='w-12'
+					/>
+					<SectionTitle title='Alberto F. Pluecker' />
+				</a>
 			</div>
 		</div>
 	)
