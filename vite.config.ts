126c1fa8be19393ae04751f9a20feb86a628e9d9
albertofp <albertopluecker@gmail.com>
2023-09-02 18:19:01 +0200

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nutrition-tracker-react',
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-    includeSource: ['src/**/*.{js,ts,tsx}'],
-    coverage: {
-      provider: 'c8',
-    },
-  },
+        base: '/',
+        plugins: [react()],
+        test: {
+                globals: true,
+                environment: 'jsdom',
+                setupFiles: ['./src/test/setup.ts'],
+                includeSource: ['src/**/*.{js,ts,tsx}'],
+                coverage: {
+                        provider: 'c8',
+                },
+        },
 })
